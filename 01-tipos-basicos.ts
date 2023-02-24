49e1a5affde09179121d84feaa2b8e97a8e85b23
efolchmontiel <e.folchm@gmail.com>
2023-02-24 18:50:41 -0300

    Initial commit

---
+++ 01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+let nombre: string = 'Strider';
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+nombre = 'Erick';
+hp = 'Full';
+
+console.log(nombre, hp);
+
+// IMPORTANTE:
+// Ver la consola de JavaScript
---
+++ README.md
@@ -0,0 +1,3 @@
+# Ejercicios-basicos-typescript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-jz7cq1)
